1ff69943cef5f53a9f07ee89e194b2ba679ce84e
TsunakawaShunya <uu.tsunakawa@gmail.com>
2024-01-06 19:57:12 +0900

    フレンド申請

---
+++ resources/views/friend/apply.blade.php
@@ -0,0 +1,27 @@
+<x-app-layout>
+    <!DOCTYPE html>
+    <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+        <head>
+            <meta charset="utf-8">
+            <title>Training</title>
+            <!-- Fonts -->
+            <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+        </head>
+        <body>
+            <div class='myid'>
+                <h1>マイID</h1>
+                {{ Auth::user()->id }} 
+            </div>
+            
+            <div class="friend-apply">
+                <form action="/friend/apply/confirm" method="POST">
+                    @csrf
+                    <h2>ID</h2>
+                    <input type="text" name="user_id" placeholder="相手のID"/>
+                    <input type="submit" value="検索">
+                </form>
+            </div>
+        </body>
+    </html>
+</x-app-layout>
+
