1a0e49a59fe4f8d7fb6a6e9a1d78eaac23f03b2b
TsunakawaShunya <uu.tsunakawa@gmail.com>
2024-01-27 17:35:55 +0900

    トップページ修正

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -10,18 +10,43 @@
         @vite(['resources/css/app.css', 'resources/js/app.js'])
     </head>
     <body class="bg-blue-300 max-h-full">
-      <header class="bg-gray-400 text-gray-800 font-bold font-mono text-center text-3xl">
-        <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
-          とれログ
-        </div>
-      </header>
-
-      <main>
-        <div class="text-gray-800 text-center text-base">
+      <header class="bg-gray-400 text-gray-800 font-bold font-mono text-3xl">
+        <div class="text-center text-base p-2 flex justify-end">
           <ul>
             <li class="p-2 text-lg font-bold"><a href="/login">ログイン</a></li>
             <li class="p-2 text-lg font-bold"><a href="/register">新規登録</a></li>
           </ul>
+        </div>
+      </header>
+
+      <main>
+        <div class="text-gray-800 font-bold font-mono text-center text-3xl p-4">
+          とれログ
+        </div>
+        
+        <div class="text-gray-800 font-bold font-mono text-center text-xl p-4 flex justify-center">
+          <ul>
+            <li>
+              筋トレに関するサービスを提供
+            </li>
+            <li>
+              このアプリ１つでトレーニングの記録やフィットネスに関する情報の取得，フレンドとの切磋琢磨ができる！
+            </li>
+          </ul>
+        </div>
+        
+        <div class="flex justify-end">
+          <div class="bg-white p-2 mx-4 my-2 w-1/4">
+            <h1 class="text-lg font-bold">テストユーザー</h1>
+            <div class="text-center">test01</div>
+            <h1 class="text-lg font-bold">メールアドレス</h1>
+            <div class="text-center">test01@mail.com</div>
+            <h1 class="text-lg font-bold">パスワード</h1>
+            <div class="text-center">test01test01</div>
+            <div class="text-end">
+              <a class="font-bold" href="/login">ログインへ</a>
+            </div>
+          </div>
         </div>
       </main>
     </body>
